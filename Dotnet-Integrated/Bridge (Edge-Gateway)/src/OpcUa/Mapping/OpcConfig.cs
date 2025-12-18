a0b259bd1d8ee2310cfdf33428a55d1450c00716
Igor Fuchs Pereira <igorfuchs111@gmail.com>
2025-12-18 19:59:35 -0300

    refactor: Multirepo

--- Dotnet-Integrated/Bridge (Edge-Gateway)/src/OpcUa/Mapping/OpcConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace OpcUa.Mapping;
-
-public static class OpcConfig
-{
-    public static Uri OPCUA_ENDPOINT { get; } = new Uri("opc.tcp://192.168.1.20:4840");
-    public static string WEBSOCKET_PREFIX { get; } = "http://localhost:5000/ws/";
-}
