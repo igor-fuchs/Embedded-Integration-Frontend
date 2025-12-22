f01111088dea7b9182126e92c1efd9e5a52addb2
Igor Fuchs Pereira <igorfuchs111@gmail.com>
2025-12-22 13:15:11 -0300

    feat: Aliases Configuration

--- React-Integrated/vite.config.ts
+++ React-Integrated/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layout': path.resolve(__dirname, 'src/components/layout'),
+      '@sections': path.resolve(__dirname, 'src/components/sections'),
+      '@simulation': path.resolve(__dirname, 'src/components/simulation'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
 })
